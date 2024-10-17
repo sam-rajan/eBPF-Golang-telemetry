ff1d38f3827b5a717c96900bb2eb3e9fc7bde03c
Sam Rajan <samjerusalem@gmail.com>
2024-10-17 23:11:23 +0100

    Add eBPF feature checking

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "eBPF-Golang-telemetry/internal/ebpf"
+import (
+	"eBPF-Golang-telemetry/internal/ebpf"
+	"errors"
+	"log"
+
+	eBPF "github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/features"
+)
 
 func main() {
+	err := features.HaveProgramType(eBPF.XDP)
+	if errors.Is(err, eBPF.ErrNotSupported) {
+		log.Fatal("eBPF not supported by the Kernel")
+	}
+
 	ebpf.Load("eth1")
 }
