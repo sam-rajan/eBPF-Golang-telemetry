a05839ccfc0721ac61aac11dffb97c8939fd6e0c
Sam Rajan <samjerusalem@gmail.com>
2024-10-17 22:37:12 +0100

    Add eBPF program for counting network packet

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "eBPF-Golang-telemetry/internal/ebpf"
 
+func main() {
+	ebpf.Load("eth1")
 }
--- internal/ebpf/gen.go
+++ internal/ebpf/gen.go
@@ -1,3 +1,3 @@
-package main
+package ebpf
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go nwpktcounter nwpktcounter.c
